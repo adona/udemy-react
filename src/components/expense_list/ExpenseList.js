3bc34ac4fa1eb0460e423f533ff9d815ffa018c2
Adona Iosif <adona.iosif@gmail.com>
2022-04-15 17:41:48 -0400

    Add expense filter

--- src/components/expense_list/ExpenseList.js
+++ src/components/expense_list/ExpenseList.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react';
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import './ExpenseList.css';
 
 function ExpenseList(props) {
+  const [year, setYear] = useState(2022);
+
+  const filterChangeHandler = (year) => {
+    setYear(year);
+  }
+
   return (
     <div className="expense-list">
+      <ExpensesFilter onFilterChange={filterChangeHandler}/>
       {props.expenses.map((expense) =>
-        <ExpenseItem key={expense.id} {...expense}/>)}
+        (expense.date.getFullYear() == year) &&
+          <ExpenseItem key={expense.id} {...expense}/>)}
     </div>
   );
 }
